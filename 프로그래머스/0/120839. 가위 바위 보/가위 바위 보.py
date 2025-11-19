272eda847411a3fadaa7edf57c96230545f833b1
030baa <030baa@gmail.com>
2025-11-19 22:10:29 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,9 +1,17 @@
 def solution(rsp):
-    answer = ''
+    dic = {'2':'0',"0":"5","5":"2"}
+    return ''.join([dic[i] for i in rsp])
+
+    
+    
+    
+    '''
+        answer = ''
     dic = {'2':'0',"0":"5","5":"2"}
     for i in rsp:
         for k,v in dic.items():
             if k == i:
                 answer += v
     return answer
-        
+
+    '''
