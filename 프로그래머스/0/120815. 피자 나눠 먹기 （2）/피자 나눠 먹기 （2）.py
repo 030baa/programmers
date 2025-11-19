7ab63cdf9da311d2a2a691083630b2a60cfa6999
030baa <030baa@gmail.com>
2025-11-19 22:49:15 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -2,3 +2,12 @@ def solution(n):
     for i in range(1,601):
         if (i*6) % n == 0:
             return i
+        
+        
+#while은 True일때 계속 돌아가는데, 0만 아니면 됨.
+# 그렇다면, 0일 때 그만 돌아간다는 거고, 나머지가 0이면 그게 답이 되니까, 그때 반환하면 됨.
+'''
+while 6 * answer % n:
+    answer += 1
+    return answer
+    '''
