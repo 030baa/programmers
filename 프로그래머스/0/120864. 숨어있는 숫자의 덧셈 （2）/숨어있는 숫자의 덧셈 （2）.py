b6b6e985075aa1686c0ba589fdbcb44a7ea466a3
030baa <030baa@gmail.com>
2025-11-20 23:44:53 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.04 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -0,0 +1,16 @@
+def solution(my_string):
+    answer = []
+    d = ''
+    for i in my_string:
+        if i.isdigit():
+            d += i
+        else:
+            if d:
+                answer.append(int(d))
+                d = ''
+    if d :
+        answer.append(int(d))
+    if answer:
+        return sum(answer)
+    else:
+        return 0
