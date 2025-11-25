17692c8feb8cc4de6f5b11091cae180dbb850637
030baa <030baa@gmail.com>
2025-11-25 23:43:52 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -0,0 +1,9 @@
+def solution(myString, pat):
+    data = ''
+    for i in myString:
+
+        if i == "A":
+            data += "B"
+        else:
+            data+= "A"
+    return 1 if pat in data else 0
